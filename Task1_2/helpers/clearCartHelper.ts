d461348086765c3865619cb781212782f4b9f982
vorobyov27 <45060447+vorobyov27@users.noreply.github.com>
2023-02-11 17:36:08 +0300

    Delete Task1_2 directory

--- Task1_2/helpers/clearCartHelper.ts
+++
@@ -1,8 +0,0 @@
-import { basket } from "../Shop/mainClass";
-
-export function clearCart() {
-    const initialArrayLenght = basket.length;
-    for (let i = 0; i < initialArrayLenght; i++) {
-        basket.shift()
-    }
-}
