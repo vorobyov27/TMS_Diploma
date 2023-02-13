cafc4c2497834322c985ac96b91797f90cce8298
Alexander <vorobyov27@gmail.com>
2023-02-13 03:34:41 +0300

    remove extra method

--- Task3/framework/elements/defaultElement.ts
+++ Task3/framework/elements/defaultElement.ts
@@ -22,10 +22,6 @@ export abstract class DefaultElement {
         return (await this.getElement()).waitForDisplayed({timeout: Config.waitTimeout, reverse :true})
     }
 
-    public async waitForClickableElement() {
-        return (await this.getElement()).waitForClickable({timeout: Config.waitTimeout})
-    }
-
     public async click() {
         (await this.getElement()).waitForClickable({timeout: Config.waitTimeout})
         return (await this.getElement()).click()
